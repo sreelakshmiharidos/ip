3dd82a1586c563b3d21667d02c49c930ec547fe9
sreelakshmiharidos <>
2025-01-30 20:37:38 +0800

    Gave a new name to the chatbot and implemented welcome and exit messages.

---
+++ src/main/java/Gilu.java
@@ -0,0 +1,14 @@
+public class Gilu {
+    public static void main(String[] args) {
+        // welcome message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Gilu");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // exit message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
